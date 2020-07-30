77738acc0a2dc9cbb2d20b7488329226aaa62f85
Krystof Spiller <krystof.spiller@gmail.com>
2020-07-30 13:58:32 +0200

    Task D done

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    if session[:counter].nil?
+      session[:counter] = 1
+    elsif session[:counter] += 1
+    end
   end
 end
