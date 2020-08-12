a0868e818d33e2b9579dc95f0c494d35f28a753a
krystof <krystof.spiller@capdesk.com>
2020-08-12 11:05:39 +0200

    Task K done

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+# React translation gem - book p 266
+gem 'i18n-js'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,10 +5,10 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
-    @products = Product.order(:title)
-    if session[:counter].nil?
-      session[:counter] = 1
-    elsif session[:counter] += 1
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
     end
   end
 end
